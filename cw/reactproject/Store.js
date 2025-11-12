a0582efcdf2a275bc19bfd7c6cb83249b789b5e5
akashcspl <akash.a@cspl.com>
2025-11-13 00:15:47 +0530

    Update Store.js

--- cw/reactproject/Store.js
+++ cw/reactproject/Store.js
@@ -1,8 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+/* import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './loginSlice';
 
 export const store = configureStore({
   reducer: {
     login: loginReducer,
+  },
+});
+ */
+
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./AuthSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
   },
 });
